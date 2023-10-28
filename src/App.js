c3780fc3bca10114cfa7831ce397a00b9042b1de
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 08:23:30 +1000

    css text import

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import Preview from './Preview';
 
-import TextCSS from './styles/00-default-template.css';
-console.log(TextCSS)
-console.log(TextCSS.toString());
+
+ // eslint-disable-next-line import/no-webpack-loader-syntax
+import css_text from '!!css-loader?{"sourceMap":false,"exportType":"string"}!./styles/00-default-template.css';
+
 
 
 function App() {
-  const test = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur, dolores animi ab laboriosam illum sint officiis error cumque vel perferendis, voluptatem porro obcaecati odit, doloremque excepturi omnis architecto doloribus? Illo exercitationem tempore optio eveniet non error provident velit nihil dolorem officiis tenetur sint explicabo similique, ipsum esse, aliquam quasi. Natus fugit in similique sed dignissimos magni quos hic alias, blanditiis harum itaque minus sint eligendi obcaecati quam enim nobis quasi sapiente temporibus repudiandae. Quam tempore, ratione corporis consectetur quis recusandae alias eius laudantium, itaque, voluptatem eveniet libero quia voluptas excepturi soluta. Illum pariatur accusamus veritatis quaerat recusandae nobis totam!';
   
-  var css_text = 'test';
+  // var css_text = 'test';
   
   return (
     <div className="App">
       <header className="App-header">
-
-
 				<div className="result_area">
 					<div className="result_css">
 						<textarea name="result_css" readOnly value={css_text}></textarea>
