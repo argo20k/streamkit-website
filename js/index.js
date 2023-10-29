5c6b41c8458bfcb376d7bd0deca2d74207b125bf
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 20:02:47 +1000

    deafult selectTempalte option
    adds first selectTemplate option to result_css

--- js/index.js
+++ js/index.js
@@ -5,26 +5,28 @@ $(document).ready(function () {
 		.then((res) => res.text())
 		.then((text) => {
 			var templates = JSON.parse(text);
-			templates.forEach((template) => {
-				// adds config template stylesheet to index.html to add it to the server files
-				document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
-				// fills selectTemplate dropdown with config template names
-				var el = document.createElement('option');
-				el.textContent = template['name'];
-				el.value = template['name'];
-				select.appendChild(el);
-			});
 
-			templates.forEach((template) => {
-				if (template['name'] === select.value) {
-					fetch(template['file_path'])
-						.then((res) => res.text())
-						.then((text) => {
-							$('#result_css').val(text);
-						})
-						.catch((e) => console.error(e));
+			for (const count in templates) {
+				if (Object.hasOwnProperty.call(templates, count)) {
+					const template = templates[count];
+					// adds config template stylesheet to index.html to add it to the server files
+					document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
+					// fills selectTemplate dropdown with config template names
+					var el = document.createElement('option');
+					el.textContent = template['name'];
+					el.value = template['name'];
+					select.appendChild(el);
+					// adds first selectTemplate option to result_css
+					if (count == 0) {
+						fetch(template['file_path'])
+							.then((res) => res.text())
+							.then((text) => {
+								$('#result_css').val(text);
+							})
+							.catch((e) => console.error(e));
+					}
 				}
-			});
+			}
 		})
 		.catch((e) => console.error(e));
 });
