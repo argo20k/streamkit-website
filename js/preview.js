f2b135958294fbd0b424f79fd38b3d0044d0036a
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 13:14:26 +1000

    file organisation and empty css files

--- js/preview.js
+++ js/preview.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
 	document.head.innerHTML +=
-		'<link rel="stylesheet" href="styles/test1.css" />';
-	// document.head.innerHTML +=
-	// 	'<link rel="stylesheet" href="styles/test2.css" />';
+		'<link rel="stylesheet" href="styles/streamkit/test1.css" />';
 
 	var links = document.getElementsByTagName('link');
 
 	for (let i = 0; i < links.length; i++) {
-		// Do stuff
 		console.log(`jimmy`, links[i].href);
 	}
 });
