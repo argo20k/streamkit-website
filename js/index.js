eb5f04c96ea72a5ec8cb90f6dcd33e9d4d97cb8c
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 21:08:54 +1000

    better config import?

--- js/index.js
+++ js/index.js
@@ -1,56 +1,16 @@
-var template_config_file = 'config.json';
+$(document).ready(async function () {
+	async function getData(url) {
+		const response = await fetch(url);
+		var text = await response.text();
+		return text;
+	}
 
-$(document).ready(function () {
-	var select = document.getElementById('selectTemplate');
-	fetch(template_config_file)
-		.then((res) => res.text())
-		.then((text) => {
-			var templates = JSON.parse(text);
+	const data = await getData('config.json');
 
-			for (const count in templates) {
-				if (Object.hasOwnProperty.call(templates, count)) {
-					const template = templates[count];
-					// adds config template stylesheet to index.html to add it to the server files
-					document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
-					// fills selectTemplate dropdown with config template names
-					var el = document.createElement('option');
-					el.textContent = template['name'];
-					el.value = template['name'];
-					select.appendChild(el);
-					// adds first selectTemplate option to result_css
-					if (count == 0) {
-						fetch(template['file_path'])
-							.then((res) => res.text())
-							.then((text) => {
-								$('#result_css').val(text);
-							})
-							.catch((e) => console.error(e));
-					}
-				}
-			}
-		})
-		.catch((e) => console.error(e));
+	// jQuery methods go here...
+	// $('#selectTemplate').hide();
+	var tempalte_list = JSON.parse(data);
+	tempalte_list.forEach((template) => {
+		document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
+	});
 });
-
-function leaveChange(control) {
-	console.log(control.value);
-
-	fetch(template_config_file)
-		.then((res) => res.text())
-		.then((text) => {
-			var templates = JSON.parse(text);
-			for (const count in templates) {
-				if (Object.hasOwnProperty.call(templates, count)) {
-					const template = templates[count];
-					if (template['name'] == control.value) {
-						fetch(template['file_path'])
-							.then((res) => res.text())
-							.then((text) => {
-								$('#result_css').val(text);
-							})
-							.catch((e) => console.error(e));
-					}
-				}
-			}
-		});
-}
