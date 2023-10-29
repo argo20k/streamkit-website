a982d890f329dd512aded6e56a447d59a1ceb00e
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 21:11:25 +1000

    straight to json instead of middle text conversion

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 $(document).ready(async function () {
 	async function getData(url) {
 		const response = await fetch(url);
-		var text = await response.text();
-		return text;
+		return response.json();
 	}
 
 	const data = await getData('config.json');
 
 	// jQuery methods go here...
 	// $('#selectTemplate').hide();
-	var tempalte_list = JSON.parse(data);
+	var tempalte_list = data;
 	tempalte_list.forEach((template) => {
 		document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
 	});
