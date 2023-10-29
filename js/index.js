caa92791b77e2652bf6ff90d46f164d39719e2c4
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-29 19:47:23 +1000

    result css dynamic to default value in dropdown

--- js/index.js
+++ js/index.js
@@ -1,26 +1,34 @@
 $(document).ready(function () {
-	var result_css_dir = 'styles/streamkit/00-default-template.css';
-	fetch(result_css_dir)
-		.then((res) => res.text())
-		.then((text) => {
-			$('#result_css').val(text);
-		})
-		.catch((e) => console.error(e));
-
+	var select = document.getElementById('selectTemplate');
 	var template_config_file = 'config.json';
 	fetch(template_config_file)
 		.then((res) => res.text())
 		.then((text) => {
 			var templates = JSON.parse(text);
 			templates.forEach((template) => {
+				// adds config template stylesheet to index.html to add it to the server files
 				document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
-
-				var select = document.getElementById('selectTemplate');
+				// fills selectTemplate dropdown with config template names
 				var el = document.createElement('option');
-				el.textContent = template.name;
-				el.value = template.name;
+				el.textContent = template['name'];
+				el.value = template['name'];
 				select.appendChild(el);
+			});
+
+			templates.forEach((template) => {
+				if (template['name'] === select.value) {
+					fetch(template['file_path'])
+						.then((res) => res.text())
+						.then((text) => {
+							$('#result_css').val(text);
+						})
+						.catch((e) => console.error(e));
+				}
 			});
 		})
 		.catch((e) => console.error(e));
 });
+
+function leaveChange(control) {
+	// console.log(control.value);
+}
