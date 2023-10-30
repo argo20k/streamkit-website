71c435bcaa58e1652f872627628bce5a2397737f
argo20k <148632221+argo20k@users.noreply.github.com>
2023-10-30 16:58:30 +1000

    documentation strings 2

--- js/preview.js
+++ js/preview.js
@@ -8,14 +8,9 @@ async function getCSSText(url) {
 	return response.text();
 }
 
-async function start() {
-	// this function runs when the page is ready
+$(document).ready(async function () {
 	const config = await getConfig('config.json');
 	var tempalte_list = config;
 	document.head.innerHTML += `<link rel="stylesheet" id="default_template_streamkit_css" href=${tempalte_list[0].file_path} />`;
 	document.head.innerHTML += `<link rel="stylesheet" id="streamkit_css" href=${tempalte_list[0].file_path} />`;
-}
-
-$(document).ready(async function () {
-	start();
 });
