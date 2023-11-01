a0935304ca1a55b8f4a6e1d48564a86369174b7f
argo20k <148632221+argo20k@users.noreply.github.com>
2023-11-01 12:51:21 +1000

    better config management
    one option config per style, not two each when it is a mod style,
    
    trying to add icons in repository

--- js/index.js
+++ js/index.js
@@ -20,63 +20,8 @@ $(document).ready(async function () {
 		return response;
 	}
 
-	function isModCheckbox(html_element_id) {
-		return /^mod_checkbox/.test(html_element_id);
-	}
-
-	function getInputValues() {
-		let option_values = [];
-		html_config.forEach((element) => {
-			if (!isModCheckbox(element['html_element_id'])) {
-				let html_value = $(`input[id=${element['html_element_id']}]`).val();
-				let option_value = {
-					id: element['id'],
-					html_element_id: element['html_element_id'],
-					html_value: html_value,
-				};
-				option_values.push(option_value);
-			}
-		});
-		return option_values;
-	}
-
-	function getModCheckboxElement(html_element) {
-		let mod_checkbox_element = '';
-		$('form input, form select').each(function (index) {
-			let input = $(this);
-			if (input.attr('name') === html_element.attr('id') && isModCheckbox(input.attr('id'))) {
-				mod_checkbox_element = input;
-			}
-		});
-		return mod_checkbox_element;
-	}
-
-	function generateHTMLConfig(default_css_text) {
-		// "label_text": "username width",
-		// "css_property_key": "username_box_width",
-		// "default_base_value": "auto",
-		// "default_mod_value": "100px",
-		// "is_mod": "true",
-		// "id": "0"
-		let html_config = [];
-		$('form input, form select').each(function (index) {
-			let input = $(this);
-			let mod_checkbox_element = getModCheckboxElement(input);
-			let mod_checkbox_id = 'none';
-			if (mod_checkbox_element) {
-				mod_checkbox_id = mod_checkbox_element.attr('id');
-			}
-
-			html_config.push({
-				id: index,
-				html_element_id: input.attr('id'),
-				mod_checkbox_id: mod_checkbox_id,
-				default_base_value: 'none',
-				default_mod_value: 'none',
-			});
-		});
-		console.log(html_config);
-		return html_config;
+	function getModCheckboxID(html_element) {
+		return `mod_checkbox_${html_element['css_property_key']}`;
 	}
 
 	function generateInputOptions() {
@@ -89,9 +34,12 @@ $(document).ready(async function () {
 
 	async function generateCSS() {
 		let adjusted_css_text = default_css_text;
-		option_values = getInputValues();
-		option_values.forEach((element) => {
-			adjusted_css_text = adjusted_css_text.replace(new RegExp(`--${element.html_element_id}:[^;]*;`), `--${element.html_element_id}: ${element.html_value};`);
+		input_values = getInputValues();
+		input_values.forEach((element) => {
+			adjusted_css_text = adjusted_css_text.replace(
+				new RegExp(`--${element.css_property_key}:[^;]*;`),
+				`--${element.css_property_key}: ${element.html_value};`
+			);
 		});
 		return adjusted_css_text;
 	}
@@ -109,15 +57,30 @@ $(document).ready(async function () {
 	}
 
 	function updateModOptions() {
-		// read only and mod option reset
-		html_config.forEach((element) => {
-			checkbox_element = $(`input[id=${element['mod_checkbox_id']}]`);
+		// read only
+		input_options_config_json.forEach((element) => {
+			checkbox_element = $(`input[id=${getModCheckboxID(element)}]`);
 			if (checkbox_element.is(':checked')) {
-				$(`input[id=${element['html_element_id']}]`).attr('readonly', false);
+				$(`input[id=${element['css_property_key']}]`).attr('readonly', false);
 			} else {
-				$(`input[id=${element['html_element_id']}]`).attr('readonly', true);
+				$(`input[id=${element['css_property_key']}]`).attr('readonly', true);
 			}
 		});
+		//	 and mod option reset (not done yet)
+	}
+
+	function getInputValues() {
+		let input_values = [];
+		input_options_config_json.forEach((element) => {
+			let html_value = $(`input[id=${element['css_property_key']}]`).val();
+			let option_value = {
+				id: element['id'],
+				css_property_key: element['css_property_key'],
+				html_value: html_value,
+			};
+			input_values.push(option_value);
+		});
+		return input_values;
 	}
 
 	//
@@ -126,12 +89,11 @@ $(document).ready(async function () {
 
 	const config_json = await (await getFile('config.json')).json();
 	const input_options_config_json = await (await getFile('input-options-config.json')).json();
-	const default_css_file = await getFile(config_json[0].file_path);
-	const default_css_text = await default_css_file.text();
+	// const default_css_text = await (await getFile(config_json[0].file_path)).text();
+	const default_css_text = await (await getFile(config_json[1].file_path)).text();
 	ApplyStylesToIndex(config_json);
 
 	generateInputOptions();
-	const html_config = generateHTMLConfig();
 	let css_text = await generateCSS();
 	updateCSSPreview(css_text);
 	updateModOptions();
@@ -147,9 +109,9 @@ $(document).ready(async function () {
 		updateCSSPreview(css_text);
 	});
 
-	// mod check box handler
-	html_config.forEach((element) => {
-		checkbox_element = $(`input[id=${element['mod_checkbox_id']}]`);
+	// mod check box event handler
+	input_options_config_json.forEach((element) => {
+		checkbox_element = $(`input[id=${getModCheckboxID(element)}]`);
 		checkbox_element.change(function () {
 			updateModOptions();
 		});
