f57038b2e7fc4113eb7212f210d0bde6e85d11e4
argo20k <148632221+argo20k@users.noreply.github.com>
2023-11-01 16:20:08 +1000

    default values
    correctly grabs default values for base + mod inputs from input config file,
    
    reverts to default values whenever mod appropriate mod checkbox is triggered

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 //
-//	foundational functions
-//
-
-// adds streamkit css styles to index.html, so the styles, and text in the files are accessible to fetch later
-function ApplyStylesToIndex(config_template_list) {
-	config_template_list.forEach((template) => {
-		document.head.innerHTML += `<link rel="stylesheet" href=${template.file_path} />`;
-	});
-}
-
-//
 //	main functions
 //
 
@@ -20,53 +9,8 @@ $(document).ready(async function () {
 		return response;
 	}
 
-	function getModCheckboxID(html_element) {
+	function getModCheckboxDOMID(html_element) {
 		return `mod_checkbox_${html_element['css_property_key']}`;
-	}
-
-	function generateInputOptions() {
-		input_options_config_json.forEach((option) => {
-			$('#input_options').append($(`<label for="mod_checkbox_${option['css_property_key']}">${option['label_text']}:</label>`));
-			$('#input_options').append($(`<input type="checkbox" name="${option['css_property_key']}" id="mod_checkbox_${option['css_property_key']}" />`));
-			$('#input_options').append($(`<input type="text" id="${option['css_property_key']}" value="${option['default_base_value']}" /><br />`));
-		});
-	}
-
-	async function generateCSS() {
-		let adjusted_css_text = default_css_text;
-		input_values = getInputValues();
-		input_values.forEach((element) => {
-			adjusted_css_text = adjusted_css_text.replace(
-				new RegExp(`--${element.css_property_key}:[^;]*;`),
-				`--${element.css_property_key}: ${element.html_value};`
-			);
-		});
-		return adjusted_css_text;
-	}
-
-	function updateCSSPreview(css_text) {
-		// update result_css
-		$('#result_css').val(css_text);
-
-		//update result_preview
-		let streamkit_css_exists = $('#result_preview').contents().find('#streamkit_css').length; // element.length is 0 if not found, 1 if found
-		if (streamkit_css_exists) {
-			$('#result_preview').contents().find('#streamkit_css').remove();
-		}
-		$('#result_preview').contents().find('head').append(`<style id='streamkit_css'>${css_text}</style>`);
-	}
-
-	function updateModOptions() {
-		// read only
-		input_options_config_json.forEach((element) => {
-			checkbox_element = $(`input[id=${getModCheckboxID(element)}]`);
-			if (checkbox_element.is(':checked')) {
-				$(`input[id=${element['css_property_key']}]`).attr('readonly', false);
-			} else {
-				$(`input[id=${element['css_property_key']}]`).attr('readonly', true);
-			}
-		});
-		//	 and mod option reset (not done yet)
 	}
 
 	function getInputValues() {
@@ -83,19 +27,68 @@ $(document).ready(async function () {
 		return input_values;
 	}
 
+	function generateInputOptions() {
+		input_options_config_json.forEach((option) => {
+			$('#input_options').append($(`<label for="mod_checkbox_${option['css_property_key']}">${option['label_text']}:</label>`));
+			$('#input_options').append($(`<input type="checkbox" name="${option['css_property_key']}" id="mod_checkbox_${option['css_property_key']}" />`));
+			$('#input_options').append($(`<input type="text" id="${option['css_property_key']}" value="${option['default_base_value']}" /><br />`));
+		});
+	}
+
+	async function generateCSS() {
+		current_css_text = default_css_text;
+		let input_values = getInputValues();
+		input_values.forEach((element) => {
+			current_css_text = current_css_text.replace(new RegExp(`--${element.css_property_key}:[^;]*;`), `--${element.css_property_key}: ${element.html_value};`);
+		});
+	}
+
+	function updateModOptions(specific_mod_element) {
+		// updating because of one element being changed (mod checkbox triggers)
+		let temp_input_options_config_json = input_options_config_json;
+		if (specific_mod_element) {
+			input_options_config_json.forEach((element) => {
+				if (element['css_property_key'] === specific_mod_element) {
+					temp_input_options_config_json = [element];
+				}
+			});
+		}
+		// updating from all elements (initial)
+		temp_input_options_config_json.forEach((element) => {
+			let mod_checkbox_element = $(`input[id=${getModCheckboxDOMID(element)}]`);
+			if (mod_checkbox_element.is(':checked')) {
+				$(`input[id=${element['css_property_key']}]`).prop('readonly', false);
+				$(`input[id=${element['css_property_key']}]`).val(element['default_mod_value']);
+			} else {
+				$(`input[id=${element['css_property_key']}]`).prop('readonly', true);
+				$(`input[id=${element['css_property_key']}]`).val(element['default_base_value']);
+			}
+		});
+	}
+
+	function updateCSSPreview() {
+		// update result_css
+		$('#result_css').val(current_css_text);
+
+		//update result_preview
+		let streamkit_css_exists = $('#result_preview').contents().find('#streamkit_css').length; // element.length is 0 if not found, 1 if found
+		if (streamkit_css_exists) {
+			$('#result_preview').contents().find('#streamkit_css').remove();
+		}
+		$('#result_preview').contents().find('head').append(`<style id='streamkit_css'>${current_css_text}</style>`);
+	}
+
 	//
 	//	inital
 	//
 
-	const config_json = await (await getFile('config.json')).json();
 	const input_options_config_json = await (await getFile('input-options-config.json')).json();
-	// const default_css_text = await (await getFile(config_json[0].file_path)).text();
-	const default_css_text = await (await getFile(config_json[1].file_path)).text();
-	ApplyStylesToIndex(config_json);
+	const default_css_text = await (await getFile('styles/streamkit/00-default.css')).text();
+	let current_css_text;
 
 	generateInputOptions();
-	let css_text = await generateCSS();
-	updateCSSPreview(css_text);
+	await generateCSS();
+	updateCSSPreview();
 	updateModOptions();
 
 	//
@@ -105,15 +98,17 @@ $(document).ready(async function () {
 	// form change event handler
 	// $('form').on('keyup change paste', 'input, select, textarea', function () {
 	$('form').on('input', async function () {
-		css_text = await generateCSS();
-		updateCSSPreview(css_text);
+		await generateCSS();
+		updateCSSPreview();
 	});
 
 	// mod check box event handler
 	input_options_config_json.forEach((element) => {
-		checkbox_element = $(`input[id=${getModCheckboxID(element)}]`);
-		checkbox_element.change(function () {
-			updateModOptions();
+		let mod_checkbox_element = $(`input[id=${getModCheckboxDOMID(element)}]`);
+		mod_checkbox_element.change(async function () {
+			updateModOptions(element['css_property_key']);
+			await generateCSS();
+			updateCSSPreview();
 		});
 	});
 });
